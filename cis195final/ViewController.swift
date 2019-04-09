3547b00cd3c98e3ef1f9fbd9ed9476653647641f
Kenny Chen <kennychen@levy-ve503-0914.apn.wlan.upenn.edu>
2019-04-09 13:44:48 -0400

    Initial Commit

---
+++ cis195final/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cis195final
+//
+//  Created by Kenny Chen on 4/9/19.
+//  Copyright © 2019 Kenny Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cis195finalTests/cis195finalTests.swift
@@ -0,0 +1,34 @@
+//
+//  cis195finalTests.swift
+//  cis195finalTests
+//
+//  Created by Kenny Chen on 4/9/19.
+//  Copyright © 2019 Kenny Chen. All rights reserved.
+//
+
+import XCTest
+@testable import cis195final
+
+class cis195finalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
